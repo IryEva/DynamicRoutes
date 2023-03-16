9a19348e2112248ebbc1ed9b015867d630483f86
Iry Eva <nivedithaharidas333@gmail.com>
2023-03-16 22:56:08 +0530

    dynamic routes task 9

--- 00-starting-setup/routes/admin.js
+++ 00-starting-setup/routes/admin.js
@@ -7,12 +7,14 @@ const adminController = require('../controllers/admin');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', adminController.getEditProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);  
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId',adminController.getEditProduct);
 
 module.exports = router;
