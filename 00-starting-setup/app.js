7c96dadceb367c78350dee520e6b6fd1a6bb0249
Iry Eva <nivedithaharidas333@gmail.com>
2023-03-29 19:04:52 +0530

    FS Booking Appointment app

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -7,27 +7,35 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
+const User = require('./models/User');
+var cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');    
-const shopRoutes = require('./routes/shop');    
+const shopRoutes = require('./routes/shop'); 
+const userRoutes = require('./routes/user');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));  
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(userRoutes);
+
 
 app.use(errorController.get404);
 
-sequelize
-  .sync()
+sequelize 
+  .sync() 
   .then(result => {
     //console.log(result);
-    app.listen(3000);
+    app.listen(4000);
 })
 .catch(err => {
     console.log(err);
---
+++ 00-starting-setup/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const userController = require('../controllers/user');
+
+const router = express.Router();
+
+router.post('/user/add-user',userController.addUser);
+
+router.get('/user/get-user',userController.getUser);
+
+router.delete('/user/delete-user/:id',userController.deleteUser);
+
+module.exports = router;
